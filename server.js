e9d27bc3178036285c264e775d4a011b0f2151cd
jorted-runner <irunner1996@gmail.com>
2025-01-27 17:02:33 -0600

    working on an ejs tutorial

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    console.log("here")
+    res.render("index", { text: "world" })
+})
+
+app.listen(8080)
