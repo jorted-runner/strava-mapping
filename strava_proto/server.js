b6d0035e7d8b09713725e23066781979b2c185ea
jorted-runner <irunner1996@gmail.com>
2025-01-28 13:01:22 -0600

    strava route

--- strava_proto/server.js
+++ strava_proto/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const stravaRouter = require('./routes/strava');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -7,7 +8,7 @@ app.use(express.json());
 app.use(logger);
 app.set('view engine', 'ejs');
 
-
+app.use('/strava', stravaRouter)
 
 function logger(req, res, next) {
 	console.log(req.originalUrl);
