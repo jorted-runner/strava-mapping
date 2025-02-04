18a6562afaf81c625cf67d689c282615526d8a50
jorted-runner <jorted.python@gmail.com>
2025-02-03 21:19:25 -0600

    Display all of the 20 activities. Display each with random color.

--- public/scripts/strava.js
+++ public/scripts/strava.js
@@ -1,16 +1,17 @@
 let map;
 
-async function initMap(polyline_data) {
+async function initMap(lat, long) {
 	//@ts-ignore
 	const { Map } = await google.maps.importLibrary('maps');
-	const { encoding } = await google.maps.importLibrary('geometry'); // Import geometry library for decodePath
 
-	const map = new Map(document.getElementById('map'), {
-		center: { lat: 39, lng: -94.28 },
+	map = new Map(document.getElementById('map'), {
+		center: { lat: lat, lng: long },
 		zoom: 12,
 	});
+}
 
-	console.log(polyline_data);
+async function createPolyline(polyline_data, color) {
+	const { encoding } = await google.maps.importLibrary('geometry'); // Import geometry library for decodePath
 
 	// Decode the polyline using Google Maps API utility
 	const path = encoding.decodePath(polyline_data); // Use the correct import for decoding
@@ -20,11 +21,11 @@ async function initMap(polyline_data) {
 		// Directly use google.maps.Polyline
 		path: path,
 		geodesic: true,
-		strokeColor: '#FF0000',
+		strokeColor: color,
 		strokeOpacity: 1.0,
 		strokeWeight: 2,
 	});
-
+	// I need to add a marker to each polyline. I want it to just show up when the user clicks on the line
 	polyline.setMap(map);
 }
 
@@ -64,11 +65,17 @@ displayButton.addEventListener('click', async (event) => {
 				throw new Error(`HTTP error! Status: ${response.status}`);
 			}
 			const data = await response.json();
+			const colors = []
+			initMap(data[0]['end_latlng'][0], data[0]['end_latlng'][1]);
 			data.forEach((activity) => {
-				activities.push(activity);
-			});
-			console.log(activities[0]);
-			initMap(activities[0].map.summary_polyline);
+				let randomColor;
+				// Generate a unique color
+				do {
+					randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+				} while (colors.includes(randomColor));
+					colors.push(randomColor);
+					createPolyline(activity.map.summary_polyline, randomColor);
+				});			
 		}
 		resetInputs();
 	} catch (error) {
